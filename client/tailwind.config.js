2d85ca6704b2bf9e870095374dd171c56f01ea68
Oleksandr <kirkovsasha009@gmail.com>
2024-06-10 11:46:45 +0300

    chore: configure tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{html,ts,tsx,scss,css}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
