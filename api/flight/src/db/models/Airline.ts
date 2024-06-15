8a9b14b0f72ce1cb5fc2149f8851bd05a39d291a
Oleksandr <kirkovsasha009@gmail.com>
2024-06-15 21:25:48 +0300

    update: airline model

--- api/flight/src/db/models/Airline.ts
+++ api/flight/src/db/models/Airline.ts
@@ -1,9 +1,17 @@
 import { Sequelize, DataTypes } from "sequelize";
-import { connection } from "db/config";
+import { connection } from "../config";
 
 export const Airline = connection.define('airline', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false,
     }
+}, {
+    tableName: 'airline',
+    timestamps: false
 })
