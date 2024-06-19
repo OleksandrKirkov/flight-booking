93444bef2a70090579c094568e7cce9cafc74155
Oleksandr <kirkovsasha009@gmail.com>
2024-06-20 00:42:16 +0300

    feat: add onClick in button interface

--- client/src/components/ui/buttons/Button.tsx
+++ client/src/components/ui/buttons/Button.tsx
@@ -1,11 +1,12 @@
-import {ReactElement, ReactText} from "react";
+import {MouseEventHandler, ReactElement, ReactText} from "react";
 import ButtonText from "./buttonText/ButtonText";
 import ButtonBorder from "./buttonBorder/ButtonBorder";
 import ButtonFill from "./buttonFill/ButtonFill";
 
 export interface IButton {
     children: ReactElement | ReactText
-    className: string | undefined
+    className?: string | undefined
+    onClick: MouseEventHandler<HTMLButtonElement>
 }
 
 export {
--- client/src/components/ui/buttons/buttonText/ButtonText.tsx
+++ client/src/components/ui/buttons/buttonText/ButtonText.tsx
@@ -2,9 +2,9 @@ import {FC} from "react"
 import {IButton} from "../Button"
 import styles from "./ButtonText.module.css"
 
-const ButtonText:FC<IButton> = ({children}) => {
+const ButtonText:FC<IButton> = ({children, onClick}) => {
     return (
-        <button className={styles.button_text}>{children}</button>
+        <button className={styles.button_text} onClick={onClick}>{children}</button>
     )
 }
 
