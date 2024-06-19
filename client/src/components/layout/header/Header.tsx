f02f9c889b71763ca71db4538cd153cad5cf6ffd
Oleksandr <kirkovsasha009@gmail.com>
2024-06-20 00:43:45 +0300

    feat: add open popup handler

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,9 +1,20 @@
 //import "./Header.css"
+import {useDispatch} from 'react-redux'
 import {ButtonText, ButtonBorder} from '../../ui/buttons/Button'
 import Container from '../container/Container'
 import styles from './Header.module.css'
+import {openPopup} from '../../../assets/store/reducers/signInSlice'
 
 const Header = () => {
+
+    const dispatch = useDispatch()
+
+    function openPopupHandler(e:any) {
+        e.preventDefault()
+
+        dispatch(openPopup())
+    }
+
     return (
         <header className={styles.header}>
             <Container className={styles.header__container}>
@@ -11,8 +22,8 @@ const Header = () => {
                     <img src="./logo/android-chrome-512x512.png" alt="logo" />
                 </a>
                 <div className={styles.buttons}>
-                    <ButtonText>Login</ButtonText>
-                    <ButtonBorder>Register</ButtonBorder>
+                    <ButtonText className='' onClick={openPopupHandler}>Login</ButtonText>
+                    <ButtonBorder className=''>Register</ButtonBorder>
                 </div>
             </Container>
         </header>
