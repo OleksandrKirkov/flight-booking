c418f4d217e28311f042ec7f6042269a43e6a90a
Oleksandr <kirkovsasha009@gmail.com>
2024-06-20 00:09:39 +0300

    feat: add search flight endpoint

--- client/src/services/flight/IFlight.ts
+++ client/src/services/flight/IFlight.ts
@@ -5,7 +5,26 @@ export interface IFlight {
     arrival_date: string
     deration: number
     price: number
-    airline_id: number
-    departure_id: number
-    arrival_id: number
+    airline: IAirline
+    departure: IAirport
+    arrival: IAirport
+}
+
+export interface IAirport {
+    id: number
+    name: string
+    city: string
+    country: string
+}
+
+export interface IAirline {
+    id: number
+    name: string
+}
+
+export interface ISearchFlight {
+    departure_city: string
+    arrival_city: string
+    departure_date: string
+    arrival_date: string
 }
--- client/src/services/flight/flight.ts
+++ client/src/services/flight/flight.ts
@@ -1,16 +1,19 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {IFlight} from "./IFlight";
+import {IFlight, ISearchFlight} from "./IFlight";
 
 export const flightApi = createApi({
     reducerPath: "flightApi",
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000/'}),
     endpoints: (builder) => ({
-        getFlight: builder.query<IFlight[], string>({
-            query: () => ({
-                url: 'flight/'
+        searchFlight: builder.query<IFlight[], ISearchFlight>({
+            query: (data) => ({
+                url: 'search-flight',
+                body: data,
+                method: 'POST'
             }),
+            transformResponse: (response: {data: IFlight[]}, meta, arg) => response.data
         })
     }) 
 })
 
-export const {useGetFlightQuery} = flightApi
+export const {useSearchFlightQuery} = flightApi
