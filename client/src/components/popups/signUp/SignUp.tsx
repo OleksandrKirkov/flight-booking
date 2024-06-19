70007ca1db3bb1ac6690c1634b4632207e103ea4
Oleksandr <kirkovsasha009@gmail.com>
2024-06-20 02:10:02 +0300

    feat: add sign up popup

---
+++ client/src/components/popups/signUp/SignUp.tsx
@@ -0,0 +1,71 @@
+import {Controller, SubmitHandler, useForm} from "react-hook-form";
+import {IAuthInput, signInField, signUpFields} from "../../../assets/forms/authFields"
+import {IoMdClose} from "react-icons/io";
+import InputBorder from "../../ui/inputs/textInput/inputBorder/inputBorder";
+import {ButtonFill} from "../../ui/buttons/Button";
+import styles from "./SignUp.module.css"
+import {useDispatch, useSelector} from "react-redux";
+import { RootState } from "../../../assets/store/store";
+import {closePopup, getPopupState} from "../../../assets/store/reducers/popupSlice";
+
+const SignUp = () => {
+
+    const popupState = useSelector((state: RootState) => getPopupState(state, 'signUp'))
+
+    const dispatch = useDispatch()
+
+    const defaultValues = signUpFields.reduce((values, field) => {
+        values[field.name as keyof IAuthInput] = "";
+        return values
+    }, {} as IAuthInput)
+
+    const {control, handleSubmit, formState: {errors}, getValues} = useForm<IAuthInput>({defaultValues: defaultValues})
+
+    const onSubmit: SubmitHandler<IAuthInput> = (data) => {
+        console.log(data, " result data")
+    }
+
+    function closePopupHandler(e:any) {
+        e.preventDefault()
+
+        dispatch(closePopup('signUp'))
+    }
+
+    return (
+        <div className={`${styles.popup} ${popupState?styles.active:''}`}>
+            <div className={styles.shadow} onClick={closePopupHandler}></div>
+
+            <form className={styles.window} onSubmit={handleSubmit(onSubmit)}>
+                <h3 className={styles.window__title}>Register</h3>
+                <button className={styles.window__close} onClick={closePopupHandler}><IoMdClose/></button>
+
+                {
+                    signUpFields.map((fieldData, index) => {
+                        return (
+                            <Controller
+                                key={index}
+                                name={fieldData.name}
+                                control={control}
+                                rules={{
+                                    ...fieldData.options
+                                }}
+                                render={({field}) => (
+                                    <InputBorder
+                                        {...field}
+                                        type={fieldData.type}
+                                        placeholder={fieldData.placeholder}
+                                        className={styles.window__input}
+                                    />
+                                )}
+                            />
+                        )
+                    })
+                }
+
+                <ButtonFill className={styles.button__submit}>Register</ButtonFill>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
