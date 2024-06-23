ccfaddb84ba23ba9119be8e32e9f91f5ec204b1d
Oleksandr <kirkovsasha009@gmail.com>
2024-06-24 00:02:06 +0300

    feat: add middlewares

---
+++ api/auth/src/middleware/errorMiddleware.ts
@@ -0,0 +1,12 @@
+import {ApiError} from "exceptions/apiError";
+import {Request, Response, NextFunction, ErrorRequestHandler} from "express";
+
+export default function (err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) {
+    console.log(err)
+
+    if(err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message, errors: err.errors})
+    }
+
+    return res.status(500).json({message: 'Непередбачувана помилка'})
+}
