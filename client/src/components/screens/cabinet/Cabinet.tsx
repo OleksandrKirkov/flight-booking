95b31bdb42121ace5837ac6e1fc5419c05bca6fb
Oleksandr <kirkovsasha009@gmail.com>
2024-06-29 19:34:06 +0300

    feat: add base structure cabinet screen

--- client/src/components/screens/cabinet/Cabinet.tsx
+++ client/src/components/screens/cabinet/Cabinet.tsx
@@ -1,6 +1,18 @@
+import {Outlet} from "react-router-dom"
+import styles from "./Cabinet.module.css"
+import Container from "../../layout/container/Container"
+import Sidebar from "./sidebar/Sidebar"
+
 const Cabinet = () => {
     return (
-        <div>Cabinet</div>
+        <div className={styles.cabinet}>
+            <Container className={styles.cabinet__container}>
+                <Sidebar />
+                <div className={styles.cabinet__screen}>
+                    <Outlet />
+                </div>
+            </Container>
+        </div>
     )
 }
 
