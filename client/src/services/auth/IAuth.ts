1aa706e2dfc53ce01162e1c51f8b73a87df9d6b8
Oleksandr <kirkovsasha009@gmail.com>
2024-07-01 23:00:10 +0300

    update: auth reducer

--- client/src/services/auth/IAuth.ts
+++ client/src/services/auth/IAuth.ts
@@ -1,6 +1,6 @@
 export interface IUser {
     email: string
-    password: string    
+    id: string    
 }
 
 export interface ISignIn {
@@ -12,4 +12,15 @@ export interface ISignIn {
 export interface ISignUp {
     email: string
     password: string
+}
+
+export interface ISignUpResult {
+    accessToken: string
+    refreshToken: string
+    user: IUser 
+}
+
+export interface ISignInResult {
+    accessToken: string
+    refreshToken: string
 }
--- client/src/services/auth/auth.ts
+++ client/src/services/auth/auth.ts
@@ -1,29 +1,84 @@
-import {fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {createApi} from "@reduxjs/toolkit/query";
-import {ISignIn, IUser} from "./IAuth";
+import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react";
+import {BaseQueryFn, FetchArgs, FetchBaseQueryError} from "@reduxjs/toolkit/query";
+import {ISignIn, ISignInResult, ISignUp, ISignUpResult, IUser} from "./IAuth";
+import {RootState} from "../../assets/store/store";
+import authSlice, {logout, setCredentials} from "../../assets/store/reducers/authSlice";
+import {useAppDispatch} from "../../assets/hooks/useRedux";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/auth',
+    prepareHeaders: (headers, { getState }) => {
+        const token = (getState() as RootState).auth.token;
+        if (token) {
+            headers.set('Authorization', `Bearer ${token}`)
+        }
+        return headers;
+   },
+   credentials: 'include'
+})
+
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    console.log('return => ', result)
+
+    if(result?.error?.status === 403) {
+        console.log('sending refresh token...')
+        
+        const refreshResult = await baseQuery('/refresh', api, extraOptions);
+        console.log('refreshResult => ', refreshResult)
+        
+        if(refreshResult?.data) {
+            const {accessToken, user} = refreshResult.data as {accessToken: string, user: IUser}
+            
+            api.dispatch(setCredentials({
+                token: accessToken,
+                user: user,
+            }))
+
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logout())
+        }
+    }
+
+    return result
+}
 
 export const authApi = createApi({
     reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000/auth'}),
-    tagTypes: ['Post'],
+    baseQuery: baseQueryWithReauth,
     endpoints: (builder) => ({
-        login: builder.mutation<IUser, ISignIn>({
+        //login
+        login: builder.mutation<ISignInResult, ISignIn>({
             query: (data) => ({
                 url: 'login',
                 method: 'POST',
                 body: data
             }),
-            transformResponse: (response: {result: number; user: IUser}) => {
-                return response.user
-            }
         }),
-
+            //transformResponse: (response: {result: number; user: ISignInResult}) => {
+                //return response.user } }),
         //register
-
+        register: builder.mutation<ISignUpResult, ISignUp>({
+            query: (data) => ({
+                url: 'registration',
+                method: 'POST',
+                body: data
+            }),
+            //transformResponse: (response: {result: number; user: ISignUpResult}) => {
+                //return response.user
+            //}
+        }),
         //logout
+        logout: builder.mutation<void, void>({
+            query: () => {
+                return {
+                    url: 'logout',
+                    method: 'POST'
+                }
+            },
+        })
     })
 })
 
-export const {
-    
-} = authApi;
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } = authApi;
